84954f78c4cd5849a972d4703c87ecdfe3dcf1fe
TasnemHatem <81652090+TasnemHatem@users.noreply.github.com>
2022-02-26 15:58:26 +0200

    UI

--- src/xogameclient/FXMLDocumentController.java
+++ src/xogameclient/FXMLDocumentController.java
@@ -8,6 +8,8 @@ package xogameclient;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +21,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -41,7 +45,8 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void didPressSingleModeButton() {
-        System.out.println("Singleeeeeeeeeeeee");
+       System.out.println("Singleeeeeeeeeeeee");
+      
     }
 
     @FXML
